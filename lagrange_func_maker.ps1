22a1f2f6637dbd446a69b2fff44b090fb1def169
BoilingNoob <84647732+BoilingNoob@users.noreply.github.com>
2024-03-01 12:43:50 -0500

    added evaluation of expression

--- lagrange_func_maker.ps1
+++ lagrange_func_maker.ps1
@@ -1,7 +1,36 @@
-Import-Module .\funcs.psm1
+Import-Module ".\funcs.psm1" -Force
 
+#https://www.geeksforgeeks.org/lagrange-interpolation-formula/
+#https://www.geeksforgeeks.org/wp-content/ql-cache/quicklatex.com-403fd0524f40b39402fb71f95fc3e7f5_l3.svg
 
 $encode_string = (Get-Content -Path .\test_string.txt)
-$points = convert_string_to_points -esncode_string $encode_string
+$points = convert_string_to_points -encode_string $encode_string
 
 $points | Format-Table
+
+$lagrange_text = make_lagrange_text -points_list $points -evaluate_Y
+
+$reped_text = replace_lagrange_text -lagrange_text $lagrange_text -points_list $points
+
+Write-Host $reped_text
+
+function evaluate_my_expression () {
+    param(
+        $expression,
+        $x,
+        [switch]$dont_round
+    )
+
+    $result = $expression -replace "X", $x | Invoke-Expression
+    if ($dont_round) {
+        $final_result = $result
+    }
+    else {
+        $final_result = [math]::Round($result, 0)
+    }
+    return $final_result
+
+}
+
+
+evaluate_my_expression -expression $reped_text -x 4
