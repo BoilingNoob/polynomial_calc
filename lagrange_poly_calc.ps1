c940348277c1a1021975b1cd483243c3bcd5e2de
BoilingNoob <84647732+BoilingNoob@users.noreply.github.com>
2024-03-01 11:47:43 -0500

    tinkering but not working

--- lagrange_poly_calc.ps1
+++ lagrange_poly_calc.ps1
@@ -1,4 +1,7 @@
-#https://mathworld.wolfram.com/LagrangeInterpolatingPolynomial.html
+#https://mathworld.wolfram.com/LagrangeInterpolatingPolynomial.html   "Written explicitly,"
+
+
+Import-Module .\funcs.psm1
 
 function make_point_obj() {
     param(
@@ -8,36 +11,12 @@ function make_point_obj() {
     return [pscustomobject]@{x = $x; y = $y }
 }
 
-function parse_point() {
-    param(
-        $input_string = $null
-    )
 
-    $coords = $input_string.split(",")
-    return make_point_obj -x $coords[0] -y $coords[1]
-}
-
-function convert_string_to_points() {
-    param(
-        $encode_string = ""
-    )
-    $encode_string = $encode_string.trim()
-    $points = New-Object System.Collections.ArrayList
-    #          =========================
-    
-    for ($i = 0; $i -lt $encode_string.length; $i++) {
-        $num_temp = [byte][char]($encode_string[$i])
-        $point_temp = make_point_obj -x ($i + 1) -y $num_temp
-        
-        $null = $points.Add($point_temp)
-    }  
-    
-    $points_array = $points -as [array]
-    return $points_array
-}
-
-
-$encode_string = (Get-Content -Path .\input_string.txt)
+#$encode_string = (Get-Content -Path .\input_string.txt)
+$encode_string = (Get-Content -Path .\test_string.txt)
 $points = convert_string_to_points -encode_string $encode_string
 
 $points | Format-Table
+
+
+lagrange_interpolation -points_list $points -test_value 2
